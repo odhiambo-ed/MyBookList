a1702434c59e40a1bab05f7cd8adb0463f346dc0
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-17 18:45:25 +0300

    Edit Linters Errors

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
 // Create a Car Class
 
 // class Car {
-//     
+//
 //      constructor (name, type) {
 //         this.name = name;
 //         this.type = type;
@@ -22,78 +22,87 @@
 
 // console.log(carOne.name)
 
-class BookList{
-    constructor(){
-        this.bookArr = [];
+class BookList {
+  constructor() {
+    this.bookArr = [];
 
-        // check if books are already stored in the localStorage
-        if (localStorage.getItem('books') !== null) {
-            // collect array of books from local storage
-            this.bookArr = JSON.parse(localStorage.getItem('books'));
+    // check if books are already stored in the localStorage
+    if (localStorage.getItem('books') !== null) {
+      // collect array of books from local storage
+      this.bookArr = JSON.parse(localStorage.getItem('books'));
 
-            //loop through books to display them to browser
-            for(let i =0; i < this.bookArr.length; i++) {
-                this.displayBook(this.bookArr[i]);
-            }
-        }
+      // loop through books to display them to browser
+      for (let i = 0; i < this.bookArr.length; i += 1) {
+        this.displayBook(this.bookArr[i].isbn);
+      }
+    }
+  }
 
-    }    
+  addBook(bookAuthor, bookTitle, bookIsbn) {
+    const book = {
+      title: bookTitle,
+      author: bookAuthor,
+      isbn: bookIsbn,
+    };
 
-    addBook(author, title, isbn){
-        const book = {
-            title: title,
-            author: author,
-            isbn: isbn,
-        }
+    this.bookArr.push(book);
 
-        this.bookArr.push(book);
+    this.displayBook(book.isbn);
 
-        this.displayBook(book);
+    localStorage.setItem('books', JSON.stringify(this.bookArr));
+  }
 
-        localStorage.setItem('books', JSON.stringify(this.bookArr));
+  displayBook(isbn) {
+    let book;
 
+    // loop through books to display them to browser
+    for (let i = 0; i < this.bookArr.length; i += 1) {
+      if (this.bookArr[i].isbn === isbn) {
+        book = this.bookArr[i];
+      }
     }
 
-    displayBook(book){
-       
-        let tr = document.createElement('tr');
-        tr.setAttribute('data-isbn', isbn);
-        tr.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td><td class = ""><a href="#" class = "btn btn-sm btn-danger delete">X</a></td>`;
+    const tr = document.createElement('tr');
+    tr.setAttribute('data-isbn', book.isbn);
+    tr.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td><td class = ""><a href="#" class = "btn btn-sm btn-danger delete">X</a></td>`;
 
-        document.querySelector('#book-list').appendChild(tr);
-    }
+    document.querySelector('#book-list').appendChild(tr);
+  }
 
-    removeBook(isbn){
-        this.bookArr = this.bookArr.filter(book => { return book.isbn != isbn });
+  removeBook(isbn) {
+    this.bookArr = this.bookArr.filter((book) => book.isbn !== isbn);
 
-        localStorage.setItem('books', JSON.stringify(this.bookArr));
-    }
+    localStorage.setItem('books', JSON.stringify(this.bookArr));
+  }
 }
 
 const bookList = new BookList();
 
 // Submit Event Listener
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const author = document.querySelector("#author").value;
-    const title = document.querySelector("#title").value;
-    const isbn = document.querySelector("#isbn").value;
+  const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const isbn = document.querySelector('#isbn').value;
 
-    bookList.addBook(author, title, isbn);
+  bookList.addBook(author, title, isbn);
 
+  document.querySelector('#author').value = '';
+  document.querySelector('#title').value = '';
+  document.querySelector('#isbn').value = '';
 });
 
 // Event listener when the user clicks on remove button
 document.addEventListener('click', (e) => {
-    let deleteButton = e.target;
-    
-    if (deleteButton !== undefined && deleteButton.classList.contains('delete')) {
-        e.preventDefault();
-        let tr = deleteButton.closest('tr');
-        let isbn = tr.getAttribute('data-isbn');
-        tr.remove();
+  const deleteButton = e.target;
 
-        bookList.removeBook(isbn);
-    }
-  });
+  if (deleteButton !== undefined && deleteButton.classList.contains('delete')) {
+    e.preventDefault();
+    const tr = deleteButton.closest('tr');
+    const isbn = tr.getAttribute('data-isbn');
+    tr.remove();
+
+    bookList.removeBook(isbn);
+  }
+});
